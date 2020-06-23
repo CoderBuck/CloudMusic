ecfd93b91021dc869824036ca89b5fd7faa3da0a
buck <coderbuck@163.com>
2020-06-23 13:58:08 +0800

    添加 drawer

--- app/src/main/java/me/buck/cloudmusic/activity/HomeActivity.kt
+++ app/src/main/java/me/buck/cloudmusic/activity/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
 import android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.core.view.updatePadding
 import me.buck.cloudmusic.Config
 import me.buck.cloudmusic.R
@@ -28,6 +29,18 @@ class HomeActivity : AppCompatActivity() {
 
     private fun initView() {
         bind.apply {
+            // drawer
+            drawerLayout.openDrawer(GravityCompat.START)
+            menu.setOnClickListener {
+                val drawerOpen = drawerLayout.isDrawerOpen(GravityCompat.START)
+                if (drawerOpen) {
+                    drawerLayout.closeDrawer(GravityCompat.START)
+                } else {
+                    drawerLayout.openDrawer(GravityCompat.START)
+                }
+            }
+
+            // viewpager
             viewPager.adapter = homeAdapter
             tabLayout.setupWithViewPager(viewPager)
             viewPager.currentItem = 1
@@ -35,7 +48,6 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private fun setupInsets() {
-        Timber.d("setupInsets")
         bind.root.systemUiVisibility = SYSTEM_UI_FLAG_LAYOUT_STABLE or SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
         bind.appbar.updatePadding(top = Config.statusBarHeight)
     }
